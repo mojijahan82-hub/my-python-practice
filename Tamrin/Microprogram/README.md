340c73951ff03a2dee37228dc453a6ad58295f5b
mojijahan82-hub <mojijahan82@gmail.com>
2026-06-22 01:17:27 +0330

    Delete Tamrin directory Agian

--- Tamrin/Microprogram/README.md
+++
@@ -1,1 +0,0 @@
-These are my Python exercises.
--- Tamrin/Microprogram/dump.py
+++
@@ -1,6 +0,0 @@
-square_input=int(input("Please Enter Number Chessboard: "))
-gains=[]
-for i in range(square_input):
-  result = 2 ** i
-  gains.append(result)
-  print(f"{gains}")
